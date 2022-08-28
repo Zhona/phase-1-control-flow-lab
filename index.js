40ee881772ec8e55ebf11717027737e22a3e8526
Teana Zhong <Teanawk@gmail.com>
2022-08-28 11:17:12 -0400

    commit tree 8594c7b052bc18670bf34f627a395412e683400c

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(aNumber){
   // Write your code here!
+  if (aNumber <= 400){
+    return 'This one is on me!'
+  } else if (aNumber > 400 && aNumber < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (aNumber > 2000 && aNumber <2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else{
+    return 'No can do.'
+  } }
+
+function ternaryCheckCity(city){
+  // Writ"e your code here!
+  let response;
+ city === 'NYC'? response = "Ok, sounds good." : response = "No go.";
+ return response;
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(money){
   // Write your code here!
+  switch (money) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default: 
+    return "Bye."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
